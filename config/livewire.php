2837c4d5261876678b0771f455e9285b86742074
Ezra Lazuardy <ezralazuardy@icloud.com>
2024-05-05 01:58:26 +0700

    feat: update

--- config/livewire.php
+++ config/livewire.php
@@ -64,16 +64,16 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => env('FILESYSTEM_DISK', 'local') === 'r2' ? 's3' : env('FILESYSTEM_DISK', 'local'),        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
-        'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
-        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
+        'disk' => env('LIVEWIRE_DISK', 'local'),    // Example: 'local', 's3'              | Default: 'default'
+        'rules' => null,                            // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'directory' => null,                        // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'middleware' => null,                       // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
+        'max_upload_time' => 5,                     // Max duration (in minutes) before an upload is invalidated...
+        'preview_mimes' => [                        // Supported file types for temporary pre-signed file URLs...
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
         ],
-        'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
     ],
 
     /*
